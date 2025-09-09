fa87095dff7929920df2d9e334bd6dc3e61c8079
Lohran Mendes <lohranfm@gmail.com>
2025-09-08 21:35:00 -0300

    initial commit

---
+++ Pages/CityManager/CreateCity.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lohran_mendes_DR4_TP2_2S.Pages.CityManager;
+
+public class CreateCity : PageModel
+{
+    [BindProperty]
+    public string? CityName { get; set; }
+
+    public void OnGet()
+    {
+    }
+    
+}
